ca358c2dfd7b758e443b50b87d50790b7d1cde8c
TukangLas21 <19623160@std.stei.itb.ac.id>
2024-12-05 00:31:09 +0700

    refactor: use correct libraries

--- src/backend/image_process/image_processing.py
+++ src/backend/image_process/image_processing.py
@@ -1,32 +1,48 @@
 import os
-import cv2
 import numpy as np
-from scipy.spatial.distance import euclidean
-from sklearn.decomposition import PCA
+from PIL import Image
+
+def get_paths(folder):
+    valid = ('.png', '.jpg', '.jpeg')
+    
+    paths = [os.path.join(folder, f) for f in os.listdir(folder) if f.endswith(valid)]
+    
+    return paths
 
 # Konversi gambar menjadi grayscale
 def convert_grayscale(image_path):
-    image = cv2.imread(image_path)
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return gray_image
+    image = Image.open(image_path).convert('RGB')
+    image_arr = np.array(image)
+    
+    if image_arr.shape[-1] == 4:
+        image_arr = image_arr[:, :, :3]
+        
+    grayscale_arr = 0.2989 * image_arr[:, :, 0] + \
+                    0.5870 * image_arr[:, :, 1] + \
+                    0.1140 * image_arr[:, :, 2]
+                    
+    grayscale_image = Image.fromarray(grayscale_arr.astype('uint8'), mode="L")
+    
+    return grayscale_image
 
-# Mengubah ukuran gambar agar seragam
-def convert_size(image, size):
-    return cv2.resize(image, size)
+# Resize gambar
+def resize_image(image, width, height):
+    return image.resize((width, height), Image.Resampling.LANCZOS)
 
-# Ubah vector grayscale menjadi 1D
-def flatten(image):
-    return image.flatten()
+# Mengubah matrix gambar menjadi 1D
+def flatten_image(image):
+    image_arr = np.array(image)
+    return image_arr.flatten()
 
-# Fungsi untuk memroses gambar-gambar dari suatu dataset
-def image_processing(image_folder, size):
+# Fungsi untuk memroses gambar-gambar dari suatu dataset menjadi array 2D
+def image_processing(image_folder, width, height):
     arr_images = []
     image_paths = [os.path.join(image_folder, f) for f in os.listdir(image_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
     
     for path in image_paths:
         grayscaled_image = convert_grayscale(path)
-        resized_image = convert_size(grayscaled_image, size)
-        flattened_image = flatten(resized_image)
+        resized_image = resize_image(grayscaled_image, width, height)
+        flattened_image = flatten_image(resized_image)
         arr_images.append(flattened_image)
         
     return np.array(arr_images)
@@ -37,32 +53,58 @@ def center_data(arr_images):
     arr_images_standardized = arr_images - mean_images
     return arr_images_standardized, mean_images
 
-# Perhitungan PCA
-def calculate_PCA(standardized_images, n):
-    pca = PCA(n_components=n)
-    pca.fit(standardized_images)
-    return pca
+# Kalkulasi PCA menggunakan SVD
+def compute_PCA(arr_images_standardized, n_components):
+    U, S, Ut = np.linalg.svd(arr_images_standardized, full_matrices=False)
+    
+    top_component = Ut[:n_components]
+    
+    projected = np.dot(arr_images_standardized, top_component.T)
+    
+    return projected, top_component
 
-def process_query(query_path, mean, pca, size):
+# Proses query (grayscale, resize, dan flattening)
+def preprocess_query(query_path, width, height):
     query_grayscale = convert_grayscale(query_path)
-    query_resized = convert_size(query_grayscale, size)
-    query_flattened = flatten(query_resized)
-    query_std = query_flattened - mean
-    query_projected = pca.transform([query_std])
-    return query_projected[0]
-
-def compute_similarity(query, dataset):
-    distances = [euclidean(query, image) for image in dataset]
-    sorted_distances = np.argsort(distances)
-    return sorted_distances, distances
-
-def image_retrieval(image_folder, query_path, n_components):
-    base_array = image_processing(image_folder, (300, 300))
-    standardized_images, mean_images = center_data(base_array)
+    query_resized = resize_image(query_grayscale, width, height)
+    flatten_query = flatten_image(query_resized)
     
-    pca = calculate_PCA(standardized_images, n_components)
-    projected_images = pca.transform(standardized_images)
+    return flatten_query
+
+# Memproyeksi gambar query menjadi bentuk vector
+def projected_query(flattened_query, mean, top_components):
+    center_query = flattened_query - mean
+    query = np.dot(center_query, top_components.T)
     
-    query_image = process_query(query_path, mean_images, pca, size=(300, 300))
-    sorted_distances, distances = compute_similarity(query_image, projected_images)
-    return sorted_distances, distances
+    return query
+
+# Mengembalikan jarak Euclidean antara query dengan gambar
+def euclidean_distance(query_vector, image_vector):
+    return np.linalg.norm(query_vector - image_vector)
+
+# Fungsi utama yang mengembalikan gambar-gambar paling mirip
+# dan jarak Euclideannya
+def image_retrieval_main(query_path, image_folder, n):
+    width, height = 300, 300
+    
+    images_path = get_paths(image_folder)
+    
+    images_array = image_processing(image_folder, width, height)
+    
+    standardized_vector, mean_images = center_data(images_array)
+    
+    n_component = 50
+    projected_vector, top_components = compute_PCA(standardized_vector, n_component)
+    
+    flattened_query = preprocess_query(query_path, width, height)
+    
+    proj_query = projected_query(flattened_query, mean_images, top_components)
+    
+    distances = np.array([euclidean_distance(proj_query, projected_vector[i]) for i in range(len(projected_vector))])
+    
+    sort_distances = np.argsort(distances)
+    top_distances = sort_distances[:n]
+    
+    top_similar = [images_path[i] for i in top_distances]
+    
+    return top_similar, top_distances
