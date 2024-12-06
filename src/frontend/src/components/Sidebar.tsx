97d0b0a42ee5e66be0f2e72b3a6bc00547f104de
Rusmin <rusmn17@gmail.com>
2024-12-06 13:00:01 +0700

    feat : upload sidebar

--- src/frontend/src/components/Sidebar.tsx
+++ src/frontend/src/components/Sidebar.tsx
@@ -1,14 +1,32 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import { useState } from "react";
 import "./Sidebar.css";
 
 const Sidebar = () => {
-  const location = useLocation(); // Mendapatkan lokasi path saat ini
+  // State untuk mengontrol apakah popup upload muncul
+  const [showUploadOptions, setShowUploadOptions] = useState(false);
+
+  // Fungsi untuk men-toggle state showUploadOptions
+  const toggleUploadOptions = () => {
+    setShowUploadOptions(!showUploadOptions);
+  };
 
   return (
     <div className="sidebar">
       <div className="placeholder">Placeholder</div>
-      <button className="sidebar-button">Upload</button>
+
+      <button className="sidebar-button" onClick={toggleUploadOptions}>
+        Upload
+      </button>
+
+      {/* Tampilkan opsi upload jika showUploadOptions true */}
+      {showUploadOptions && (
+        <div className="upload-options">
+          <button className="sidebar-button">Album Images</button>
+          <button className="sidebar-button">Music Audios</button>
+          <button className="sidebar-button">Mapper</button>
+        </div>
+      )}
+
       <button className="sidebar-button">Pictures</button>
       <button className="sidebar-button">Audios</button>
     </div>
